f9bacccf6cd53001f70d9e4e782d3d2aacc9b137
Jaime Muñoz <jaime.munoz200@gmail.com>
2020-10-10 10:22:59 -0600

    Se crea componente app

---
+++ src/containers/App.svelte
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<style>
+    :global(body){
+        background-color: #676778;
+        color: white;
+    }
+</style>
+
+<div class="main">
+    <h1>Hello Pugstagram</h1>
+</div>
