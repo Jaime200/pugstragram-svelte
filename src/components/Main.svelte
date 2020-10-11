cb2fd7105bd2ea089b2c8f9c07371a207fa009ed
Jaime Muñoz <jaime.munoz200@gmail.com>
2020-10-10 19:08:29 -0600

    Se agrega css al componente Main

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -2,6 +2,21 @@
 
 </script>
 
+<style>
+    .Main {
+      display: grid;
+      grid-template-columns: minmax(auto, 936px);
+      justify-content: center;
+      background-color: #fafafa;
+    }
+    .Main__container {
+      display: grid;
+      grid-template-columns: 3fr 1fr;
+      grid-gap: 2em;
+      padding: 2em 0;
+    }
+  </style>
+  
 <div class="Main">
     <div class="Main__container">
         <slot />
