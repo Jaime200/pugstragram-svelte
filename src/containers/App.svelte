74cb7742da738b1c1c06aac88ae68d923454d134
Jaime Muñoz <jaime.munoz200@gmail.com>
2020-10-10 19:34:08 -0600

    Se modifico las fuentes en el css

--- src/containers/App.svelte
+++ src/containers/App.svelte
@@ -6,8 +6,8 @@ import Sidebar from '../components/Sidebar.svelte'
 </script>
 
 <style>
-  @import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400&display=swap');
-  @import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400&display=swap');
+    @import url("https://fonts.googleapis.com/css?family=Pacifico&display=swap");
+  @import url("https://fonts.googleapis.com/css?family=Lato:300,400,600&display=swap");
     :global(body){
         background-color: #fafafa;
         color: rgba(38, 38, 38, 0.7);
