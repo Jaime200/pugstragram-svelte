168d887445466f6da6c3fab14bb31813d602d914
Jaime Muñoz <jaime.munoz200@gmail.com>
2020-10-11 10:26:05 -0600

    Estilos css en Footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,7 +1,15 @@
 <script>
 
 </script>
-
+<style>
+    .Footer {
+      font-size: 11px;
+      letter-spacing: 1px;
+      font-weight: normal;
+      color: #c7c7c7;
+    }
+  </style>
+  
 <div class="Footer">
     <div class="Footer__container">
         <div class="Footer__copy">
