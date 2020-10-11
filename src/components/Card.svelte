e526738e740c72a5d7820afa620ca1bb8c9da081
Jaime Muñoz <jaime.munoz200@gmail.com>
2020-10-11 10:25:24 -0600

    Estilos CSS en card

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -2,19 +2,136 @@
 import Comments from './Comments.svelte'
 </script>
 
+
+<style>
+    .Card {
+      border: 1px solid rgba(219, 219, 219, 1);
+      border-radius: 4px;
+      background-color: white;
+      margin: 0 0 2em 0;
+    }
+    .Card__Header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 1em;
+    }
+    .Card__user {
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+    }
+    .Card__user img {
+      width: 32px;
+      height: 32px;
+      border-radius: 50%;
+    }
+    .Card__user h2 {
+      margin: 0;
+      padding: 0;
+      font-size: 14px;
+      font-weight: 600;
+      margin: 0 0 0 1em;
+      color: black;
+    }
+    .Card__user h2 span {
+      display: block;
+      font-size: 12px;
+      font-weight: normal;
+      color: rgba(38, 38, 38, 0.7);
+    }
+    .Card__photo {
+      padding: 0;
+      margin: 0;
+    }
+    .Card__photo img {
+      width: 100%;
+      height: auto;
+    }
+    .Card__photo figure {
+      margin: 0;
+      padding: 0;
+      cursor: pointer;
+    }
+    .Card__settings i {
+      cursor: pointer;
+    }
+    .Card__icons {
+      padding: 1em;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+    .Card__icons i {
+      margin: 0 1em 0 0;
+      cursor: pointer;
+      font-size: 20px;
+    }
+    .Card__icons i:last-child {
+      margin: 0;
+    }
+    .Card__description {
+      padding: 0 1em 1em 1em;
+    }
+    .Card__description h3 {
+      font-size: 14px;
+      font-weight: bold;
+      color: black;
+    }
+    .Card__description span {
+      font-size: 14px;
+    }
+    .active__like {
+      color: #bc1888;
+      animation: bounce linear 0.8s;
+      animation-iteration-count: 1;
+      transform-origin: 20% 20%;
+    }
+    .active__bookmark {
+      color: #f09433;
+    }
+  
+    @keyframes bounce {
+      0% {
+        transform: translate(0px, 0px);
+      }
+      15% {
+        transform: translate(0px, -25px);
+      }
+      30% {
+        transform: translate(0px, 0px);
+      }
+      45% {
+        transform: translate(0px, -15px);
+      }
+      60% {
+        transform: translate(0px, 0px);
+      }
+      75% {
+        transform: translate(0px, -5px);
+      }
+      100% {
+        transform: translate(0px, 0px);
+      }
+    }
+  </style>
+
+
 <div class="Card">
     <div class="Card__container">
-        <div class="Card__header">
+        <div class="Card__Header">
             <div class="Card__user">
                 <img src="https://arepa.s3.amazonaws.com/elmo002.jpg" alt="">
-                <h2>Elmo.pug</h2>
-                <span>Bogota Colombia</span>
+                <h2>Elmo.pug
+                  <span>Bogota Colombia</span>
+                </h2>
+                
             </div>            
             <div class="Card__settings">
                 <i class='fas fa-ellipsis-h' ></i>
             </div>
         </div>
-        <div class="Car__photo">
+        <div class="Card__photo">
             <figure>
                 <img src="https://arepa.s3.amazonaws.com/elmo002.jpg" alt="">
             </figure>
@@ -31,9 +148,9 @@ import Comments from './Comments.svelte'
 
         <div class="Card__description">
             <h3>elmo.pug</h3>
-            <span>Hola</span>
-            <Comments/>
+            <span>Hola</span>            
         </div>
+        <Comments/>
           
     </div>
 </div>
