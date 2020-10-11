4e1ec11b30d1c0d31157b051bd5fa9d7b3b0f6a7
Jaime Muñoz <jaime.munoz200@gmail.com>
2020-10-10 19:08:01 -0600

    Se agrega css al componente Header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -5,13 +5,16 @@
 
 .Header {
     background-color: white;
-    border-bottom: 1px solid rgba(38, 38, 38, 0.4);
+    position: fixed;
+    width: 100%;    
 }
 
 .Header__container{
     display: grid;
     justify-content: center;
-    grid-template-columns: minmax(auto, 936px)
+    grid-template-columns: minmax(auto, 936px);
+    background-color: white;
+    border-bottom: 1px solid rgba(38, 38, 38, 0.4);
 }
 
 .Header__content{
@@ -19,17 +22,42 @@ display: flex;
 flex-direction: row;
 justify-content: space-between;
 align-items: center;
+padding: 0.5em;
 }
 
 .Header__nav ul{
-    display: flex;
-    flex-direction: row;
     list-style-type: none;
 }
 
 .Header__nav ul li {
-    margin-right: 20px;
+    display: inline-block;
+    margin: 0 0 0 1em;
 }
+
+h1 {
+    padding: 0;
+    margin: 0;
+    font-size: 28px;
+    color: black;
+    font-family: "Pacifico", cursive;
+    cursor: pointer;
+    position: relative;
+    background: #fff;
+    mix-blend-mode: multiply;
+    display: inline-block;
+  }
+
+  h1:before {
+    content: "";
+    display: block;
+    width: 100%;
+    height: 100%;
+    background: radial-gradient(circle at top left, #f09433, #bc1888);
+    mix-blend-mode: screen;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
 </style>
 <header class="Header">
     <div class="Header__container">
