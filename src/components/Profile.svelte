6bfbe26853f788bdc539ea256ced20e9476f217c
Jaime Muñoz <jaime.munoz200@gmail.com>
2020-10-11 10:26:25 -0600

    Estilos css en Profile

--- src/components/Profile.svelte
+++ src/components/Profile.svelte
@@ -1,11 +1,35 @@
 <script>
 
 </script>
+<style>
+    .Profile__content {
+      display: flex;
+      align-items: center;
+    }
+    .Profile__avatar img {
+      width: 42px;
+      height: 42px;
+      border-radius: 50%;
+    }
+    .Profile__info {
+      margin: 0 0 0 0.5em;
+    }
+    .Profile__info h2 {
+      font-size: 14px;
+      color: black;
+      margin: 0;
+      padding: 0;
+    }
+    .Profile__info span {
+      font-size: 12px;
+      font-weight: normal;
+    }
+  </style>
 
 <div class="Profile">
     <div class="Profile__content">
         <div class="Profile__avatar">
-            <img src="https://arepa.s3.amazonaws.com/oscar.png" alt="">
+            <img src="https://arepa.s3.amazonaws.com/oscar.png" alt="foto">
         </div>
         <div class="Profile__info">
             <h2>Jaime</h2>
